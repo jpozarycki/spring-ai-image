d764ce86cbfe7d028ba3ee9b46cd33c351b4613a
jpozarycki <jakub.pozarycki@gmail.com>
2024-05-30 19:32:07 +0200

    implementing openai image client

--- src/main/java/com/jpozarycki/springaiimage/services/OpenAIServiceImpl.java
+++ src/main/java/com/jpozarycki/springaiimage/services/OpenAIServiceImpl.java
@@ -2,13 +2,28 @@ package com.jpozarycki.springaiimage.services;
 
 import com.jpozarycki.springaiimage.model.Question;
 import lombok.RequiredArgsConstructor;
+import org.springframework.ai.image.ImageOptionsBuilder;
+import org.springframework.ai.image.ImagePrompt;
+import org.springframework.ai.openai.OpenAiImageModel;
 import org.springframework.stereotype.Service;
+
+import java.util.Base64;
 
 @RequiredArgsConstructor
 @Service
 public class OpenAIServiceImpl implements OpenAIService {
+    private final OpenAiImageModel imageClient;
     @Override
     public byte[] getImage(Question question) {
-        return new byte[0];
+        var imageOptions = ImageOptionsBuilder.builder()
+                .withHeight(1024)
+                .withWidth(1024)
+                .withResponseFormat("b64_json")
+                .build();
+        var b64string = imageClient.call(new ImagePrompt(question.question(), imageOptions))
+                .getResult()
+                .getOutput()
+                .getB64Json();
+        return Base64.getDecoder().decode(b64string);
     }
 }
