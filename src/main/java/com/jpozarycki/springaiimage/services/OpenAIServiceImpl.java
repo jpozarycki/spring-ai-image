08c9ebd062ad47627da04534745c535ee43db2d8
jpozarycki <jakub.pozarycki@gmail.com>
2024-05-30 19:44:04 +0200

    openai specific options

--- src/main/java/com/jpozarycki/springaiimage/services/OpenAIServiceImpl.java
+++ src/main/java/com/jpozarycki/springaiimage/services/OpenAIServiceImpl.java
@@ -2,9 +2,9 @@ package com.jpozarycki.springaiimage.services;
 
 import com.jpozarycki.springaiimage.model.Question;
 import lombok.RequiredArgsConstructor;
-import org.springframework.ai.image.ImageOptionsBuilder;
 import org.springframework.ai.image.ImagePrompt;
 import org.springframework.ai.openai.OpenAiImageModel;
+import org.springframework.ai.openai.OpenAiImageOptions;
 import org.springframework.stereotype.Service;
 
 import java.util.Base64;
@@ -15,10 +15,12 @@ public class OpenAIServiceImpl implements OpenAIService {
     private final OpenAiImageModel imageClient;
     @Override
     public byte[] getImage(Question question) {
-        var imageOptions = ImageOptionsBuilder.builder()
+        var imageOptions = OpenAiImageOptions.builder()
                 .withHeight(1024)
                 .withWidth(1024)
                 .withResponseFormat("b64_json")
+                .withModel("dall-e-3")
+                .withQuality("hd")
                 .build();
         var b64string = imageClient.call(new ImagePrompt(question.question(), imageOptions))
                 .getResult()
